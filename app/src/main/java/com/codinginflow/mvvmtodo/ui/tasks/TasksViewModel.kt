5ddebc5b408efe05893f7b17e7a5677550e277b9
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2022-01-27 10:36:21 +0300

    add tasks adapter and optionsmenu in tollbar

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -2,9 +2,19 @@ package com.codinginflow.mvvmtodo.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.codinginflow.mvvmtodo.data.TaskDao
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flatMapLatest
 
 class TasksViewModel @ViewModelInject constructor(
     val taskDao: TaskDao
 ): ViewModel() {
+    val searchQuery = MutableStateFlow("")
+
+    private val tasksflow = searchQuery.flatMapLatest {
+        taskDao.getTasks(it)
+    }
+
+    val task = tasksflow.asLiveData()
 }
