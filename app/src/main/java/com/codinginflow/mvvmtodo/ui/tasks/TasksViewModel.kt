0bd90d2cfea607787f5f45ce36f4ac22eea95270
DennisMMugambi <dennis.mutwiri.mugambi@gmail.com>
2022-01-28 10:40:36 +0300

    add jetpack datastore

--- app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/ui/tasks/TasksViewModel.kt
@@ -3,30 +3,39 @@ package com.codinginflow.mvvmtodo.ui.tasks
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.codinginflow.mvvmtodo.data.PreferencesManager
+import com.codinginflow.mvvmtodo.data.SortOrder
 import com.codinginflow.mvvmtodo.data.TaskDao
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.flatMapLatest
+import kotlinx.coroutines.launch
 
 class TasksViewModel @ViewModelInject constructor(
-    val taskDao: TaskDao
+    val taskDao: TaskDao,
+    private val preferencesManager: PreferencesManager
 ): ViewModel() {
     val searchQuery = MutableStateFlow("")
 
-    val sortOrder = MutableStateFlow(SortOrder.BY_DATE)
-    val hideCompleted = MutableStateFlow(false)
+    val preferencesFlow = preferencesManager.preferencesFlow
 
     private val tasksflow = combine(
         searchQuery,
-        sortOrder,
-        hideCompleted
-    ) { query, sortOrder, hideCompleted ->
-        Triple(query, sortOrder, hideCompleted)
-    }.flatMapLatest{(query, sortOrder, hideCompleted) ->
-        taskDao.getTasks(query, sortOrder, hideCompleted)
+        preferencesFlow
+    ) { query, filterPrefences ->
+        Pair(query, filterPrefences)
+    }.flatMapLatest{(query, filterPrefences) ->
+        taskDao.getTasks(query, filterPrefences.sortOrder,filterPrefences.hideCompleted)
+    }
+
+    fun onSortOrderSelected(sortOrder: SortOrder) = viewModelScope.launch {
+        preferencesManager.updateSortOrder(sortOrder)
+    }
+
+    fun onHideCompletedSelected(hideCompleted: Boolean) = viewModelScope.launch {
+        preferencesManager.updateHideCompleted(hideCompleted)
     }
 
     val task = tasksflow.asLiveData()
 }
-
-enum class SortOrder{BY_NAME, BY_DATE}
